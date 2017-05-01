84a148d98518563c9e5deeeb7b555a2ae3718bdf
wholden <holdenwm523@gmail.com>
2017-05-01 14:47:11 -0700

    should be functional now

--- arduinostepper/arduinostepper.py
+++ arduinostepper/arduinostepper.py
@@ -2,8 +2,16 @@ import serial
 import time
 import re
 
-sampleserial = serial.Serial('/dev/ttyACM1',57600,timeout=0.001)
-cameraserial = serial.Serial('/dev/ttyACM2',57600,timeout=0.001)
+motordict = {'camera':{'devname':'camera','stepsperrev':200,'leadscrewpitch':0.635,'microstep':16},
+            'sample':{'devname':'sample','stepsperrev':200,'pulleyratio':16/60,'microstep':16}}
+motordict['camera']['mmperstep'] = motordict['camera']['leadscrewpitch']/motordict['camera']['stepsperrev']/motordict['camera']['microstep']
+motordict['sample']['degperstep'] = 360*motordict['sample']['pulleyratio']/motordict['sample']['stepsperrev']/motordict['sample']['microstep']
+
+serialaddresses = ['/dev/ttyACM0','/dev/ttyACM1']
+
+# serial0 = serial.Serial('/dev/ttyACM0',57600,timeout=0.001)
+# serial1 = serial.Serial('/dev/ttyACM1',57600,timeout=0.001)
+initialize_motors()
 
 class arduinoMotor:
 
@@ -25,7 +33,59 @@ class arduinoMotor:
             r = ''
         return r
 
-samplemotor = arduinoMotor(sampleserial)
-assert samplemotor.sendrecv('DN')=='sample', 'sample motor error: name is not \'sample\''
-cameramotor = arduinoMotor(cameraserial)
-assert cameramotor.sendrecv('DN')=='camera', 'camera motor error: name is not \'camera\''
+def initialize_motors():
+    for ad in serialaddresses:
+        ser = serial.Serial(ad,57600,timeout=0.001)
+        motorhandle = arduinoMotor(ser)
+        k = motorhandle.sendrecv('DN')
+        if k is in motordict:
+            motordict[k]['handle'] = motorhandle
+        print('Motor with name '+k+' is initialized'.)
+
+def go_to_degree(degree):
+    """Move sample motor to angular position indicated by degree.
+    
+    This moves the sample rotator to the indicated position.
+    Current implementation sets degree=0 to location at 
+    powerup of the controller. Zero location can be reset
+    by sending 'PX=0' string to the controller.
+
+    Angular position is calculated based on configuration
+    in motordict dictionary.  Calculation makes use of
+    'pulleyratio', 'stepsperrev', and 'microstep'.
+
+    Args:
+        degree: Degree of position to rotate to.
+    """
+    steps = degree/motordict['sample']['degperstep']
+    xstr='X'+str(int(steps))
+    SendRecv(motordict['sample']['handle'],xstr)
+
+def go_to_mm(distance):
+    """Move camera motor to position indicated by distance(mm).
+    
+    This moves the camera stage to the indicated position.
+    Make sure to zero the controller out by using the 'L-'
+    command string, so that distance=0 is properly calibrated.
+
+    Distance (mm) is calculated based on configuration
+    in motordict dictionary.  Calculation makes use of
+    'leadscrewpitch', 'stepsperrev', and 'microstep'.
+
+    Args:
+        distance: Distance in mm to move the camera to.
+    """
+    steps = distance/motordict['camera']['mmperstep']
+    xstr="X"+str(int(steps))
+    print(xstr)
+    SendRecv(motordict['camera']['handle'],xstr)
+
+def get_camera_position():
+    steps = int(SendRecv(motordict['camera']['handle'],'PX')[1])
+    distance = steps*motordict['camera']['mmperstep']
+    return str(round(distance,2))+'mm'
+
+def get_sample_position():
+    steps = int(SendRecv(motordict['sample']['handle'],'PX')[1])
+    theta = steps*motordict['sample']['degperstep']
+    return str(round(theta,2))+'deg'
